46644ac429f2f7d8fe71eea6318129940b1026b7
Muthu1809 <muthu1809@gmail.com>
2023-05-15 13:20:48 +0530

    eclips commit

---
+++ myproject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author muthu
+ *
+ */
+module myproject {
+}
---
+++ myproject/src/mypackage/Home.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+public class Home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
